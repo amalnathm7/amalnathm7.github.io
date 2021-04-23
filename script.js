9ee4c20a35d27e089e68b917e5e690f0a9395ad4
amalnathm7 <amalnathm7@gmail.com>
2021-04-23 15:01:21 +0530

    Web design

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ const scrollBtn = document.querySelector(".scrollToTop-btn");
 window.addEventListener("scroll", function(){
     scrollBtn.classList.toggle("active", window.scrollY > 500);
 });
+
+scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+
+    for(var i=0; i < reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 50;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add("active");
+        }
+    }
+}
